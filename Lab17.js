c99f7c85b2a0b9abcc371d9083bd3c0a283b4a04
NuggetIsHere <jessicarsuarez14@gmail.com>
2024-11-18 15:07:35 -0500

    Update Lab17.js

--- Lab17.js
+++ Lab17.js
@@ -1,54 +1,57 @@
 "use strict";
 var gl;
 var points;
-var texSize = 64; 
 
-// Create a checkerboard pattern using floats. This was added
-var image1 = new Array() 
-for (var i =0; i<texSize; i++)  image1[i] = new Array(); 
-for (var i =0; i<texSize; i++) 
-for ( var j = 0; j < texSize; j++) 
-image1[i][j] = new Float32Array(4); 
-for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) { 
-var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)); 
-image1[i][j] = [c, c, c, 1]; 
-} 
+var texSize = 64;
+// Create a checkerboard pattern using floats
+var image1 = new Array()
+    for (var i =0; i<texSize; i++)  image1[i] = new Array();
+    for (var i =0; i<texSize; i++)
+        for ( var j = 0; j < texSize; j++)
+           image1[i][j] = new Float32Array(4);
+    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
+        var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0));
+        image1[i][j] = [c, c, c, 1];
+    }
 
-// Convert floats to ubytes for texture 
-var image2 = new Uint8Array(4*texSize*texSize); 
-for (var i = 0; i < texSize; i++) 
-for (var j = 0; j < texSize; j++) 
-for(var k =0; k<4; k++) 
-image2[4*texSize*i+4*j+k] = 255*image1[i][j][k]; 
+// Convert floats to ubytes for texture
+var image2 = new Uint8Array(4*texSize*texSize);
+    for (var i = 0; i < texSize; i++)
+        for (var j = 0; j < texSize; j++)
+           for(var k =0; k<4; k++)
+                image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
 
-var texCoordsArray = []; 
-var texCoord = [ 
-vec2(0, 0), 
-vec2(0, 1), 
-vec2(1, 1) 
+var texCoordsArray = [];
+//full texture
+
+// perpendicular texture w/isoceles triangle
+//
+var texCoord = [
+    vec2(0.5, 1),
+    vec2(0  , 0),
+    vec2(1  , 0)
 ];
+//
 
 var positionsArray = [];
 var colorsArray = [];
-var vertexColor = vec4( 0.0 , 1.0 , 1.0 , 1.0);  
+var vertexColor = vec4(0.0, 0.0, 1.0, 1.0);  
 
 window.onload = init;
 
-function configureTexture(image) { 
-var texture = gl.createTexture(); 
-gl.activeTexture(gl.TEXTURE0); 
-gl.bindTexture(gl.TEXTURE_2D, texture); 
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,        
-gl.RGBA, gl.UNSIGNED_BYTE, image); 
-gl.generateMipmap(gl.TEXTURE_2D); 
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,          
-gl.NEAREST_MIPMAP_LINEAR); 
-//        gl.NEAREST_MIPMAP_NEAREST); 
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, 
-gl.NEAREST); 
-//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, 
-gl.NEAREST_MIPMAP_LINEAR); 
-} 
+function configureTexture(image) {
+    var texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,
+        gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+          gl.NEAREST_MIPMAP_LINEAR);
+//        gl.NEAREST_MIPMAP_NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+//    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+}
 
 function init()
 {
@@ -57,13 +60,16 @@ function init()
     gl = canvas.getContext('webgl2');
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+
 // isoceles triangle
+//
     points=[
     vec4(  0.00 ,  0.95 , 0.0 , 1.0 ),
     vec4( -0.95 , -0.95 , 0.0 , 1.0 ),
     vec4(  0.95 , -0.95 , 0.0 , 1.0 )
-    ];
 
+    ];
+//
     positionsArray.push(points[0]);
     colorsArray.push(vertexColor);
     positionsArray.push(points[1]);
@@ -71,8 +77,8 @@ function init()
     positionsArray.push(points[2]);
     colorsArray.push(vertexColor);
 
-    texCoordsArray.push(texCoord[0]); 
-    texCoordsArray.push(texCoord[1]); 
+    texCoordsArray.push(texCoord[0]); //
+    texCoordsArray.push(texCoord[1]);
     texCoordsArray.push(texCoord[2]);
     
     //
@@ -80,7 +86,6 @@ function init()
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
 
     //  Load shaders and initialize attribute buffers
 
@@ -115,7 +120,7 @@ function init()
 
     configureTexture(image2);
 
-    gl.uniform1i( gl.getUniformLocation(program, "uTextureMap"), 0); 
+    gl.uniform1i( gl.getUniformLocation(program, "uTextureMap"), 0);
 
     render();
 };
